3ae6b73206d0da44da2f07a57fb5c22a250b4353
Are4 <41898367+Are4@users.noreply.github.com>
2020-08-17 19:39:07 +0300

    Update PySys.py

--- PySys.py
+++ PySys.py
@@ -4,6 +4,10 @@ import time
 from random import randint
 import os
 
+battery = "1"
+batteryint = int(battery)
+batteryloss = 0
+
 
 def clear():
     if name == 'nt':
@@ -15,7 +19,6 @@ def clear():
 
 if os.path.exists("logs"):
     os.remove("logs")
-stoplog = 0
 logdelete = 0
 print("__________         _________             ")
 print("\______   \___.__./   _____/__.__. ______")
@@ -41,47 +44,83 @@ print("")
 print("Script loaded!")
 time.sleep(2)
 clear()
-print("Welcome to PySys. An operating system made with python.")
+print("Welcome to PySys. An operating system made with python.                                                             ", battery, "%", sep="")
 print("Type 'cmd' to get started\n")
-firststoplog = 0
-line = 0
-data = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-        "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "",
-        "", "", "", ""]
+changestoplog = 0
+bchargeline = 0
 
 while True:
-    if line == 200:
-        if firststoplog == 0:
-            print("You have used more than 200 commands and the logs file won't save any more commands upon restarting the"
-                    " script.\n")
-            stoplog = 1
-            firststoplog = 1
+    if bchargeline == 1:
+        bchargeline = 0
+    if batteryint == 0:
+        clear()
+        print("You have run out of battery! Charging...")
+        flb = open("logs", "a")
+        thing = "    Batteries depleted and charged automatically\n"
+        bchargeline = 1
+        flb.write(thing)
+        flb.close()
+        time.sleep(0.1)
+        animation = ["0%", "5%", "20%", "23%", "39%", "43%", "50%", "72%", "85%", "100%"]
+        for i in range(len(animation)):
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            time.sleep(0.5)
+            sys.stdout.flush()
+        clear()
+        battery = 100
+        batteryint = int(battery)
+        batteryloss = 0
+        print("Battery succesfully charged! Restarting...")
+        time.sleep(2)
+        clear()
+        print("Welcome to PySys. An operating system made with python.                                                             ", battery, "%", sep="")
+        print("Type 'cmd' to get started\n")
     DONTDELETE = 0
     cmd = input(">")
-    if stoplog == 0:
-        if cmd != "logclear":
-            fl = open("logs", "w")
-            data[line] = "User issued the command '" + cmd + "'\n"
-            fl.writelines(data)
-            line = line + 1
+    batteryloss = batteryloss + 1
+    if batteryloss == 10:
+        batteryloss = 0
+        batteryint = batteryint - 1
+        battery = str(batteryint)
+    if cmd != "logclear":
+        if bchargeline == 0:
+            fl = open("logs", "a")
+            content = "User issued the command '" + cmd + "'\n"
+            fl.write(content)
             fl.close()
+    lines = lines + 1
     if cmd == "cmd":
         print("List of available commands:")
-        print("cmd, sd, clr, about, contact, randnr, artwork, createhelp, readhelp, delhelp, logread\n")
+        print("cmd, sd, clr, about, contact, randnr, artwork, createhelp, readhelp, delhelp, logread, rf, recharge\n")
+    elif cmd == "recharge":
+        clear()
+        print("Charging battery...")
+        time.sleep(0.1)
+        animation = ["0%", "1%", "5%", "20%", "23%", "39%", "50%", "72%", "85%", "100%"]
+        for i in range(len(animation)):
+            sys.stdout.write("\r" + animation[i % len(animation)])
+            time.sleep(0.5)
+            sys.stdout.flush()
+        clear()
+        battery = 100
+        batteryint = int(battery)
+        batteryloss = 0
+        print("Battery succesfully charged! Restarting...")
+        flb = open("logs", "a")
+        content = "    Batteries recharged\n"
+        flb.write(content)
+        flb.close()
+        time.sleep(2)
+        clear()
+        print("Welcome to PySys. An operating system made with python.                                                             ", battery, "%", sep="")
+        print("Type 'cmd' to get started\n")
     elif cmd == "sd":
         conf = input("Type 'y' to confirm: ")
         if conf == "y":
-            if stoplog == 0:
-                flc = open("logs", "w")
-                data[line] = "    User issued the confirmation command '" + conf + "'\n"
-                flc.writelines(data)
-                line = line + 1
-                flc.close()
+            flc = open("logs", "a")
+            content = "    User issued the confirmation command '" + conf + "'\n"
+            flc.write(content)
+            flc.close()
             animation = ["Shutting down in 3", "Shutting down in 2", "Shutting down in 1"]
             for i in range(len(animation)):
                 sys.stdout.write("\r" + animation[i % len(animation)])
@@ -89,16 +128,18 @@ while True:
                 sys.stdout.flush()
             break
         else:
-            if stoplog == 0:
-                flc = open("logs", "w")
-                data[line] = "    User issued the confirmation command '" + conf + "'\n"
-                flc.writelines(data)
-                line = line + 1
-                flc.close()
+            flc = open("logs", "a")
+            content = "    User issued the confirmation command '" + conf + "'\n"
+            flc.write(content)
+            flc.close()
             print("System not shut down\n")
     elif cmd == "clr":
         clear()
-        print("Welcome to PySys. An operating system made with python.")
+        print("Welcome to PySys. An operating system made with python.                                                             ", battery, "%", sep="")
+        print("Type 'cmd' to get started\n")
+    elif cmd == "rf":
+        clear()
+        print("Welcome to PySys. An operating system made with python.                                                             ", battery, "%", sep="")
         print("Type 'cmd' to get started\n")
     elif cmd == "about":
         print("You are using PySys 1.1 made by Are4\n")
@@ -106,19 +147,15 @@ while True:
         print("If you want to send the creator a message, you can do so on Discord to Are4#2139\n")
     elif cmd == "randnr":
         fromnr = input("Generate a random number from ")
-        if stoplog == 0:
-            flnr = open("logs", "w")
-            data[line] = "    User issued the number command '" + fromnr + "'\n"
-            flnr.writelines(data)
-            line = line + 1
-            flnr.close()
+        flnr = open("logs", "a")
+        content = "    User issued the number command '" + fromnr + "'\n"
+        flnr.write(content)
+        flnr.close()
         tonr = input("to ")
-        if stoplog == 0:
-            flnr = open("logs", "w")
-            data[line] = "    User issued the number command '" + tonr + "'\n"
-            flnr.writelines(data)
-            line = line + 1
-            flnr.close()
+        flnr = open("logs", "a")
+        content = "    User issued the number command '" + tonr + "'\n"
+        flnr.write(content)
+        flnr.close()
         print("")
         nrcheck = fromnr + tonr
         not_allowed_list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -138,12 +175,10 @@ while True:
             if continuee1 == 1:
                 gen = randint(fromnrint, tonrint)
                 print("The random number is", gen)
-                if stoplog == 0:
-                    fls = open("logs", "w")
-                    data[line] = "    Script said the following '" + tonr + "'\n"
-                    fls.writelines(data)
-                    line = line + 1
-                    fls.close()
+                fls = open("logs", "a")
+                content = "    Script said the following '" + tonr + "'\n"
+                fls.write(content)
+                fls.close()
             if continuee1 == 0:
                 print("Your from number value is higher than the to number value")
         if continuee == 0:
@@ -164,22 +199,18 @@ while True:
         print("And on the second line after it says 'Write content:', write what you want to be stored.\n")
     elif cmd == "create":
         filename = input("Filename: ")
-        if stoplog == 0:
-            flf = open("logs", "w")
-            data[line] = "    User gave the following filename '" + filename + "'\n"
-            flf.writelines(data)
-            line = line + 1
-            flf.close()
+        flf = open("logs", "a")
+        content = "    User gave the following filename '" + filename + "'\n"
+        flf.write(content)
+        flf.close()
         content = input("Write content: ")
         f = open(filename, "w")
         f.write(content)
         f.close()
-        if stoplog == 0:
-            fls = open("logs", "w")
-            data[line] = "    Script created the file '" + filename + "'\n"
-            fls.writelines(data)
-            line = line + 1
-            fls.close()
+        fls = open("logs", "a")
+        content = "    Script created the file '" + filename + "'\n"
+        fls.write(content)
+        fls.close()
         print("Text file successfully created\n")
     elif cmd == "readhelp":
         print("With this command you can read a text file which you created with the 'create' command.")
@@ -187,22 +218,18 @@ while True:
         print("And then, after it says 'filename:', input the name of the text file\n")
     elif cmd == "read":
         filename = input("Filename: ")
-        if stoplog == 0:
-            flf = open("logs", "w")
-            data[line] = "    User gave the following filename '" + filename + "'\n"
-            flf.writelines(data)
-            line = line + 1
-            flf.close()
+        flf = open("logs", "a")
+        content = "    User gave the following filename '" + filename + "'\n"
+        flf.write(content)
+        flf.close()
         if os.path.exists(filename):
             f = open(filename, "r")
             content = f.read()
             f.close()
-            if stoplog == 0:
-                fls = open("logs", "w")
-                data[line] = "    Script read the file '" + filename + "'\n"
-                fls.writelines(data)
-                line = line + 1
-                fls.close()
+            fls = open("logs", "a")
+            content2 = "    Script read the file '" + filename + "'\n"
+            fls.write(content2)
+            fls.close()
             print("The text file had the following message: ")
             print("'", content, "'", sep="")
         else:
@@ -215,12 +242,10 @@ while True:
         print("Then, confirm with a 'y' that you want to delete it and it will be deleted.\n")
     elif cmd == "del":
         filename = input("Filename: ")
-        if stoplog == 0:
-            flf = open("logs", "w")
-            data[line] = "    User gave the following filename '" + filename + "'\n"
-            flf.writelines(data)
-            line = line + 1
-            flf.close()
+        flf = open("logs", "a")
+        content = "    User gave the following filename '" + filename + "'\n"
+        flf.write(content)
+        flf.close()
         if filename == "PySys.py" or filename == "pysys.py" or filename == "Pysys.py" or filename == "pySys.py"\
                 or filename == "pYsys.py" or filename == "pysYs.py" or filename == "pysyS.py" or filename == "pysys.Py"\
                 or filename == "pysys.pY" or filename == "PYsys.py" or filename == "PysYs.py" or filename == "PysyS.py"\
@@ -230,12 +255,10 @@ while True:
                 or filename == "PYSys.pY" or filename == "PYSYS.py" or filename == "PYSYs.Py" or filename == "PYSYs.pY"\
                 or filename == "PYSYS.Py" or filename == "PYSYS.pY" or filename == "PYSYS.PY":
             print("You can't delete this python script. Please don't try to.")
-            if stoplog == 0:
-                fle = open("logs", "w")
-                data[line] = "    Script did not delete the file '" + filename + "'\n"
-                fle.writelines(data)
-                line = line + 1
-                fle.close()
+            fle = open("logs", "a")
+            content = "    Script did not delete the file '" + filename + "'\n"
+            fle.write(content)
+            fle.close()
             DONTDELETE = 1
         if filename == "Run.bat" or filename == "run.bat" or filename == "rUn.bat" or filename == "ruN.bat"\
                 or filename == "run.Bat" or filename == "run.bAt" or filename == "run.baT" or filename == "RUn.bat"\
@@ -244,53 +267,43 @@ while True:
                 or filename == "RUN.Bat" or filename == "RUN.bAt" or filename == "RUN.baT" or filename == "RUN.BAt"\
                 or filename == "RUN.BaT" or filename == "RUN.BAT":
             print("You can't delete this file. Please don't try to.")
-            if stoplog == 0:
-                fle = open("logs", "w")
-                data[line] = "    Script did not delete the file '" + filename + "'\n"
-                fle.writelines(data)
-                line = line + 1
-                fle.close()
+            fle = open("logs", "a")
+            content = "    Script did not delete the file '" + filename + "'\n"
+            fle.write(content)
+            fle.close()
             DONTDELETE = 1
         if filename == "logs":
             print("You can't delete this file. Please don't try to.")
-            if stoplog == 0:
-                fle = open("logs", "w")
-                data[line] = "    Script did not delete the file '" + filename + "'\n"
-                fle.writelines(data)
-                line = line + 1
-                fle.close()
+            fle = open("logs", "a")
+            content = "    Script did not delete the file '" + filename + "'\n"
+            fle.write(content)
+            fle.close()
             DONTDELETE = 1
         if DONTDELETE == 0:
             if os.path.exists(filename):
                 confirm = input("Type 'y' to confirm that action: ")
-                if stoplog == 0:
-                    flc = open("logs", "w")
-                    data[line] = "    User issued the confirmation command '" + confirm + "'\n"
-                    flc.writelines(data)
-                    line = line + 1
-                    flc.close()
+                flc = open("logs", "a")
+                content = "    User issued the confirmation command '" + confirm + "'\n"
+                flc.write(content)
+                flc.close()
                 if confirm == "y":
                     os.remove(filename)
-                    if stoplog == 0:
-                        fls = open("logs", "w")
-                        data[line] = "    Script deleted the file '" + filename + "'\n"
-                        fls.writelines(data)
-                        line = line + 1
-                        fls.close()
+                    fls = open("logs", "a")
+                    content = "    Script deleted the file '" + filename + "'\n"
+                    fls.write(content)
+                    fls.close()
                     print("File deleted.")
                 else:
                     print("File not deleted")
             else:
                 print("That file doesn't exist.")
-                if stoplog == 0:
-                    fls = open("logs", "w")
-                    data[line] = "    Script said the file '" + filename + "' doesn't exist\n"
-                    fls.writelines(data)
-                    line = line + 1
-                    fls.close()
+                fls = open("logs", "a")
+                content = "    Script said the file '" + filename + "' doesn't exist\n"
+                fls.write(content)
+                fls.close()
         print("")
     elif cmd == "logread":
-        logread = open("logs", "r")
+        logread = open("logs", "r+")
         logs = logread.read()
         logread.close()
         print("")
